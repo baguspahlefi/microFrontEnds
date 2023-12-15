85c8311de7c647cf0108c688e78ffd27c3eadc5e
Bagus Pahlefi <baguspahlefi2000@gmail.com>
2023-12-15 14:06:48 +0700

    chart

--- projects/mfe/src/app/chart/chart.component.ts
+++ projects/mfe/src/app/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-chart',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent {
+  public chart: any;
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
+      type: 'bar',
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+
+ 
+
 
 }
