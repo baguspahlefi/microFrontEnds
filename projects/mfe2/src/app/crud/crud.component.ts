0ed3d56b34e3911d496ceaae720a9356eaaa6f2c
Bagus Pahlefi <baguspahlefi2000@gmail.com>
2023-12-16 01:43:19 +0700

    CRUD

--- projects/mfe2/src/app/crud/crud.component.ts
+++ projects/mfe2/src/app/crud/crud.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,20 +8,159 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CrudComponent implements OnInit {
 
-    public todos: any[] = [];
-    li: any;
-    lis = [];
-    constructor(private http: HttpClient) {
- 
+  public todos: any[] = [];
+  public post: any;
+  public updatedPost: any = {};
+  public loading = true;
+  public showInput = false;
+  public selectedTodoId: number | null = null;
+  public titleError: string | null = null;
+  public bodyError: string | null = null;
+
+  @ViewChild('titleInput') titleInput!: ElementRef;
+  @ViewChild('bodyInput') bodyInput!: ElementRef;
+
+  constructor(private http: HttpClient) {}
+
+  toggleInput() {
+    this.showInput = !this.showInput;
+
+    if (this.showInput) {
+      setTimeout(() => {
+        this.titleInput.nativeElement.focus();
+      }, 0);
     }
-    ngOnInit(): void {
-        this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
-          this.li = data;
-          console.log(this.li);
-          this.lis = this.li.list;
-          console.log(this.lis);
-        });
-    
-        
+  }
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (data: any) => {
+        this.todos = data;
+        this.loading = false;
+      },
+      (error) => {
+        console.error(error);
+        this.loading = false;
+      }
+    );
+  }
+
+
+
+  addTodo() {
+
+    const title = this.titleInput.nativeElement.value;
+    const body = this.bodyInput.nativeElement.value;
+
+    if (!title || !body) {
+      this.titleError = !title ? 'Title is required.' : null;
+      this.bodyError = !body ? 'Body is required.' : null;
+      return;
     }
+
+    // Reset errors
+    this.titleError = null;
+    this.bodyError = null;
+
+    this.titleInput.nativeElement.value = '';
+    this.bodyInput.nativeElement.value = '';
+    this.showInput = false;
+  }
+
+  submitTodo() {
+    const newTodo = {
+      title: this.titleInput.nativeElement.value,
+      body: this.bodyInput.nativeElement.value,
+      userId: 1,
+    };
+
+    this.http.post('https://jsonplaceholder.typicode.com/posts', newTodo).subscribe(
+      (response) => {
+        console.log('Data berhasil ditambah:', response);
+        this.post = response;
+      },
+      (error) => {
+        console.error('Error menambah data', error);
+      }
+    );
+
+    // Reset the input fields and hide them
+    this.titleInput.nativeElement.value = '';
+    this.bodyInput.nativeElement.value = '';
+    this.showInput = false;
+  }
+
+  openUpdateModal(todoId: number) {
+    this.selectedTodoId = todoId;
+  }
+
+  submitTodoUpdate() {
+    const title = this.titleInput.nativeElement.value;
+    const body = this.bodyInput.nativeElement.value;
+
+    if (!title || !body) {
+      this.titleError = !title ? 'Title is required.' : null;
+      this.bodyError = !body ? 'Body is required.' : null;
+      return;
+    }
+
+    // Reset errors
+    this.titleError = null;
+    this.bodyError = null;
+
+    if (this.selectedTodoId) {
+      const updatedTodo = {
+        title: this.titleInput.nativeElement.value,
+        body: this.bodyInput.nativeElement.value,
+        userId: 1,
+      };
+  
+      this.http.put(`https://jsonplaceholder.typicode.com/posts/${this.selectedTodoId}`, updatedTodo)
+        .subscribe(
+          (response) => {
+            console.log('Data berhasil diupdate:', response);
+            this.refreshData();
+            $(`#basicModal-${this.selectedTodoId}`).modal('hide');
+          },
+          (error) => {
+            console.error('Error mengupdate data', error);
+          }
+        );
+  
+  
+      this.titleInput.nativeElement.value = '';
+      this.bodyInput.nativeElement.value = '';
+      this.showInput = false;
+      this.selectedTodoId = null; 
+    } else {
+      this.addTodo();
+    }
+  }
+
+  refreshData() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (data: any) => {
+        this.todos = data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  deleteTodo(todoId: number) {
+    this.http.delete(`https://jsonplaceholder.typicode.com/posts/${todoId}`,{ observe: 'response' })
+      .subscribe(
+        (response) => {
+          console.log(`Data ID ${todoId} berhasil dihapus`+ `Status :` + response.status);
+          // Remove the deleted todo from the local array
+          this.todos = this.todos.filter(todo => todo.id !== todoId);
+        },
+        (error) => {
+          console.error(error `${todoId}`);
+        }
+      );
+  }
+
+
 }
--- projects/mfe2/src/app/crud/crud.module.ts
+++ projects/mfe2/src/app/crud/crud.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CrudComponent } from './crud.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forChild([
       {path: "", component:CrudComponent, pathMatch:"full"}
       ])
