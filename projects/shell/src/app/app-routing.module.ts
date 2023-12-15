cb1c4ea8ed534e2376c77e362f2dd75fbe507b44
Bagus Pahlefi <baguspahlefi2000@gmail.com>
2023-12-15 20:23:57 +0700

    dynamic module

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,25 +1,36 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: HomeComponent,
-    pathMatch: "full"
+    pathMatch: 'full',
   },
   {
-    path: "chart-list",
-    loadChildren: () => import('mfe/ChartModule').then(m => m.ChartModule),
+    path: 'chart-list',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:3000/remoteEntry.js',
+        exposedModule: './ChartModule',
+      }).then((m) => m.ChartModule),
   },
   {
-    path: "crud",
-    loadChildren: () => import('mfe2/CrudModule').then(m => m.CrudModule),
-  }
+    path: 'crud',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4000/remoteEntry.js',
+        exposedModule: './CrudModule',
+      }).then((m) => m.CrudModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -3,8 +3,7 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    mfe: 'http://localhost:3000/remoteEntry.js',
-    mfe2: 'http://localhost:4000/remoteEntry.js',    
+   
   },
 
   shared: {
